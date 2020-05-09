372f37a00c61eefbdfbc067293f4fbfd8b3f4f48
Artem Shibaev <2fasti.miem@gmail.com>
2020-05-09 16:58:40 +0300

    Fix transpose error

--- get_ecg_features.py
+++ get_ecg_features.py
@@ -13,37 +13,30 @@ def _set_duration(waveform):
     else:
         return waveform
 
-
 def _zero_pad(waveform, align):
     """Zero pad waveform (align: left, center, right)."""
     # Get remainder
     remainder = LENGTH - len(waveform)
-
-    padded_waveform = np.zeros((12, 5000))
-
     if align == 'left':
         for lead in range(LEADS_NUM):
-            wave = np.pad(waveform[:, lead], (0, remainder), 'constant', constant_values=0).reshape(1, -1)
-            padded_waveform[lead, :] = wave
-        return padded_waveform
+            np.pad(waveform[:, lead], (0, remainder), 'constant', constant_values=0)
+        return waveform
     elif align == 'center':
         for lead in range(LEADS_NUM):
-            wave = np.pad(waveform[:, lead], (int(remainder / 2), remainder - int(remainder / 2)), 'constant', constant_values=0)
-            padded_waveform[lead, :] = wave
-        return padded_waveform
+            np.pad(waveform[:, lead], (int(remainder / 2), remainder - int(remainder / 2)), 'constant', constant_values=0)
+        return waveform
     elif align == 'right':
         for lead in range(LEADS_NUM):
-            wave = np.pad(waveform[:, lead], (remainder, 0), 'constant', constant_values=0)
-            padded_waveform[lead, :] = wave
-        return padded_waveform
+            return np.pad(waveform[:, lead], (remainder, 0), 'constant', constant_values=0)
+        return waveform
 
 
 def get_ecg_features(data):
     ecg = ECG(file_name=None, label=None,
-              waveform=data, filter_bands=[3, 45], fs=500)
+              waveform=data.transpose(), filter_bands=[3, 45], fs=500)
 
     # Set waveform duration
     waveform = _set_duration(waveform=ecg.filtered)
     waveform_pad = _zero_pad(waveform=waveform, align='center')
 
-    return waveform_pad.reshape(-1, LEADS_NUM)
+    return waveform_pad
